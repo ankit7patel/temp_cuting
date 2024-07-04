38e58f5eecad51deb0b36633701484c1a21456ca
ankittt9 <ankitptl7024@gmail.com>
2024-07-04 19:21:22 +0530

    temolate cuting

---
+++ project/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('home/',home,name='home'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact')
+
+
+   
+]
